7f4b1a9f1df825cf5e51c502e4e4dc13a30fbc11
gaofengJ <760613910@qq.com>
2022-01-02 02:02:24 +0800

    [update]添加随机点餐功能

---
+++ src/data/config.ts
@@ -0,0 +1,3 @@
+const menus: string[] = ['炸鸡', '肉夹馍', '肯德基', '麻辣香锅'];
+
+export { menus };
--- src/page/order/index.tsx
+++ src/page/order/index.tsx
@@ -1,9 +1,51 @@
 import React, { useState } from 'react';
-
+import { useDidMount } from 'hooooks';
+import { menus } from '@/data/config';
+import { Button } from 'antd-mobile';
 import './index.less';
 
+let timer: any = null;
+
 const Order = () => {
-  return <div style={{ color: 'red' }}>猪猪猪</div>;
+  const [menuArr, setMenuArr] = useState<string[]>([]);
+  const [index, setIndex] = useState<number>(0);
+  const [isOrdering, setIsOrdering] = useState<boolean>(false);
+
+  const handleOrder = () => {
+    if (isOrdering) {
+      console.log('isOrdering', isOrdering);
+      clearInterval(timer);
+      setIsOrdering(false);
+      return;
+    }
+    setIsOrdering(true);
+    const len = menuArr.length;
+    if (!len) return;
+    timer = setInterval(() => {
+      const num = Math.floor(Math.random() * (len + 1));
+      setIndex(num);
+    }, 50);
+  };
+
+  useDidMount(() => {
+    const menuStorage: string[] = JSON.parse(
+      localStorage.getItem('menus') || '[]',
+    );
+    setMenuArr(menuStorage.length ? menuStorage : menus);
+  });
+
+  return (
+    <div className='order-page'>
+      <div>还在犹豫吗？马上随机点餐，拯救选择困难症~</div>
+      <div className='menu-content'>{menuArr[index]}</div>
+      <div className='button-wrapper'>
+        <Button color='primary' onClick={handleOrder}>
+          {isOrdering ? '停止点菜' : '开始点菜'}
+        </Button>
+        <Button color='primary'>修改菜单</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Order;
